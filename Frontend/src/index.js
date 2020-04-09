70938d3b45978b603a2483ae9344777df19e68e3
sheenagupta <sheena.gupta.in@gmail.com>
2020-04-09 15:47:40 -0700

    trying theme

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// core components
+import Admin from "layouts/Admin.js";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import "assets/css/material-dashboard-react.css?v=1.8.0";
+
+const hist = createBrowserHistory();
+
+ReactDOM.render(
+<Router history={hist}>
+    <Switch>
+    <Route path="/admin" component={Admin} />
+<Redirect from="/" to="/admin/dashboard" />
+    </Switch>
+    </Router>,
+document.getElementById("root")
+);
