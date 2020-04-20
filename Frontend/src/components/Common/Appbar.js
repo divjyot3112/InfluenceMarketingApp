fd031abc2aa2c55b8e9f93e0b6993e19ca3d8320
divjyot3112 <divjyot.khanuja@sjsu.edu>
2020-04-20 11:53:38 -0700

    Seperated nav and sidebar

--- Frontend/src/components/Common/Appbar.js
+++ Frontend/src/components/Common/Appbar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
-// UI imports
-import { slide as Menu} from 'react-burger-menu';
+// Importing Sidebar
+import Sidebar from './Sidebar'
+// UI component imports
 import {
     Collapse,
     Navbar,
@@ -9,16 +10,16 @@ import {
     Nav,
     NavItem,
     NavLink,
-    UncontrolledDropdown,
-    DropdownToggle,
-    DropdownMenu,
-    DropdownItem,
+    // UncontrolledDropdown,
+    // DropdownToggle,
+    // DropdownMenu,
+    // DropdownItem,
     NavbarText,
     Input,
     Button,
     InputGroupAddon,
     InputGroup,
-    InputGroupText,
+    // InputGroupText,
 } from 'reactstrap';
 import { Image, } from 'react-bootstrap'
 import { FaUserAlt, FaSignOutAlt } from 'react-icons/fa' 
@@ -26,32 +27,28 @@ import { FaUserAlt, FaSignOutAlt } from 'react-icons/fa'
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { getProfile } from "../../actions/userProfileActions";
-// CSS
-import "./styles.css";
-
 
 // Beginning of class
 export class Appbar extends Component {
     state = {
         isOpen: false,
         userExists: false,
-        profile: null
+        firstName: null
     }
-
     componentWillMount() {
         if(localStorage.getItem('email')!=null) {
             this.props.getProfile(localStorage.getItem('email'), (response) => {
                 console.log(response.status)
                 if(response.status === 200) {
                     this.setState({
-                        profile: response.data.message,
+                        firstName: response.data.message.name?response.data.message.name.firstName : "",
                         userExists: true
                     })
                 }
             })
-            this.setState({
-                profile: this.props.profile
-            })
+            // this.setState({
+            //     profile: this.props.profile
+            // })
         } else {
             this.setState({
                 userExists: false
@@ -60,7 +57,7 @@ export class Appbar extends Component {
     }
 
     // For toggling open and close of Navbar
-    toggle = () => {
+    toggle = (e) => {
         this.setState({
             isOpen: !this.state.isOpen
         })
@@ -68,56 +65,49 @@ export class Appbar extends Component {
 
     render() {
         let username = null;
-        let role = null
-        console.log(this.state.profile)
+        console.log(this.state.firstName)
         if(this.state.userExists) {
-            role = this.state.profile.role
-            username = <NavLink href="/profile">{this.state.profile.name?this.state.profile.name.firstName:"Hello"}</NavLink>
+            username = <NavLink href="/profile">{this.state.firstName?this.state.firstName:"Hello"}</NavLink>
         } else {
             username = "Login or SignUp"
         }
-        let dashLink = role ? (role=="Influencer" ? "/dashboard/influencer" : "/dashboard/sponsor" ) : "/"
-        let rateLink = role ? (role=="Influencer" ? "/ratings/influencer" : "/ratings/sponsor" ) : "/" 
         return (
             <div>
                 <Navbar color="light" light expand="md">
                     <NavbarBrand href="/">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Influence Marketing App</NavbarBrand>
                     <NavbarToggler onClick={this.toggle} />
-                    <Collapse isOpen={this.state.isOpen} navbar></Collapse>
-                    <Nav className="mr-auto" navbar>
-                        <NavItem style={{width:350}}>
-                            <InputGroup>
-                                <Input 
-                                    type="text" 
-                                    className="form-control" 
-                                    placeholder="Search People and Jobs"
-                                />
-                                <InputGroupAddon addonType="append">
-                                    <Button color="secondary">
-                                        Go
-                                    </Button>
-                                </InputGroupAddon>
-                            </InputGroup>
-                        </NavItem>
-                        <NavItem style={{marginLeft:25}}>
-                            <FaUserAlt style={{height:40}} /> {/* To do -> add profile picture of user */}
-                        </NavItem>
-                        <NavItem style={{marginLeft:25, background:"#dadee3"}}>
-                            { username }
-                        </NavItem>
-                        <NavItem style={{marginLeft:20, color:"#ff9191"}}>
-                            Sign Out <FaSignOutAlt style={{height:40}} /> {/*Redirect to signout*/}
-                        </NavItem >
-                    </Nav>
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="mr-auto" navbar>
+                            <NavItem style={{width:350, marginLeft:25}}>
+                                <InputGroup>
+                                    <Input 
+                                        type="text" 
+                                        className="form-control" 
+                                        placeholder="Search People and Jobs"
+                                        // height={}
+                                    />
+                                    <InputGroupAddon addonType="append">
+                                        <Button color="secondary">
+                                            Go
+                                        </Button>
+                                    </InputGroupAddon>
+                                </InputGroup>
+                            </NavItem>
+                        </Nav>
+                        <Nav navbar>
+                            <NavItem style={{marginLeft:25}}>
+                                <FaUserAlt style={{height:40}} /> {/* To do -> add profile picture of user */}
+                            </NavItem>
+                            <NavItem style={{marginLeft:25, background:"#dadee3"}}>
+                                { username }
+                            </NavItem>
+                            <NavItem style={{marginLeft:25, color:"#ff9191"}}>
+                                <NavLink>Sign Out <FaSignOutAlt style={{}} /></NavLink> {/*Redirect to signout*/}
+                            </NavItem >
+                        </Nav>
+                    </Collapse>
                 </Navbar>
-                <Menu>
-                    <a className='menu-item' href='/'>Home</a>
-                    <a className='menu-item' href='/profile'>Profile</a>
-                    <a className='menu-item' href={dashLink}>Dashboard</a>
-                    <a className='menu-item' href="/">Analytics</a>
-                    <a className='menu-item' href='/inbox'>Inbox</a>
-                    <a className='menu-item' href={rateLink}>My Ratings</a>
-                </Menu>                
+                <Sidebar />
             </div>
         )
     }
@@ -126,11 +116,7 @@ export class Appbar extends Component {
 // Defining props
 Appbar.protoTypes = {
     getProfile: PropTypes.func.isRequired,
-    profile: PropTypes.object.isRequired
+    // profile: PropTypes.object.isRequired
 }
 
-const mapStateToProps = (state) => ({
-    profile: state.profile
-});
-
-export default connect(mapStateToProps, { getProfile } ) (Appbar);
+export default connect(null, { getProfile } ) (Appbar);
