b6580be0c1d2dd313d3f4e3c9e1526194abb6bad
sheenagupta <sheena.gupta.in@gmail.com>
2020-04-22 00:52:43 -0700

    navbar fix

--- Frontend/src/components/Common/Appbar.js
+++ Frontend/src/components/Common/Appbar.js
@@ -27,6 +27,7 @@ import {FaUserAlt, FaSignOutAlt} from 'react-icons/fa'
 import {connect} from "react-redux";
 import PropTypes from "prop-types";
 import {getProfile} from "../../actions/userProfileActions";
+import "../../css/appbar.css";
 
 // Beginning of class
 export class Appbar extends Component {
@@ -54,7 +55,7 @@ export class Appbar extends Component {
     }
 
     // For toggling open and close of Navbar
-    toggle = (e) => {
+    toggle = () => {
         this.setState({
             isOpen: !this.state.isOpen
         })
@@ -64,47 +65,48 @@ export class Appbar extends Component {
         let username = null;
 
         if (this.state.userExists) {
-            username = <NavLink href="/profile">{this.state.firstName ? this.state.firstName : "Hello"}</NavLink>
+            username = <NavLink href="/profile">{this.state.firstName ? this.state.firstName : ""}</NavLink>
         }
 
         return (
-            <div>
-                <Navbar color="light" light expand="md">
-                    <NavbarBrand href="/">Together</NavbarBrand>
-                    <NavbarToggler onClick={this.toggle}/>
-                    <Collapse isOpen={this.state.isOpen} navbar>
-                        <Nav className="mr-auto" navbar>
-                            <NavItem style={{width: 350, marginLeft: 25}}>
-                                <InputGroup>
-                                    <Input
-                                        type="text"
-                                        className="form-control"
-                                        placeholder="Search People and Jobs"
-                                        // height={}
-                                    />
-                                    <InputGroupAddon addonType="append">
-                                        <Button variant="outline-success">
-                                            Go
-                                        </Button>
-                                    </InputGroupAddon>
-                                </InputGroup>
-                            </NavItem>
-                        </Nav>
-                        <Nav navbar>
-                            <NavItem style={{marginLeft: 25}}>
-                                <FaUserAlt style={{height: 40}}/> {/* To do -> add profile picture of user */}
-                            </NavItem>
-                            <NavItem style={{marginLeft: 25, background: "#dadee3"}}>
-                                {username}
-                            </NavItem>
-                            <NavItem style={{marginLeft: 25, color: "#ff9191"}}>
-                                <NavLink>Sign Out <FaSignOutAlt style={{}}/></NavLink> {/*Redirect to signout*/}
-                            </NavItem>
-                        </Nav>
-                    </Collapse>
-                </Navbar>
-                {/* <Sidebar /> */}
-            </div>
+            <React.Fragment>
+                <div>
+                    <Navbar color="light" light expand="md">
+                        <NavbarBrand href="/" className="navbar-brand">Together</NavbarBrand>
+                        <NavbarToggler onClick={this.toggle}/>
+                        <Collapse isOpen={this.state.isOpen} navbar>
+                            <Nav className="mr-auto" navbar>
+                                <NavItem style={{width: 350, marginLeft: 25}}>
+                                    <InputGroup>
+                                        <Input
+                                            type="text"
+                                            className="form-control"
+                                            placeholder="Search People and Tasks"
+                                        />
+                                        <InputGroupAddon addonType="append">
+                                            <Button variant="outline-success">
+                                                Go
+                                            </Button>
+                                        </InputGroupAddon>
+                                    </InputGroup>
+                                </NavItem>
+                            </Nav>
+                            <Nav navbar>
+                                <NavItem style={{marginLeft: 25}}>
+                                    <FaUserAlt style={{height: 40}}/> {/* To do -> add profile picture of user */}
+                                </NavItem>
+                                <NavItem style={{marginLeft: 25, background: "#dadee3"}}>
+                                    {username}
+                                </NavItem>
+                                <NavItem style={{marginLeft: 25, color: "#ff9191"}}>
+                                    <NavLink>Sign Out <FaSignOutAlt style={{}}/></NavLink> {/*Redirect to signout*/}
+                                </NavItem>
+                            </Nav>
+                        </Collapse>
+                    </Navbar>
+                    {/* <Sidebar /> */}
+                </div>
+            </React.Fragment>
         )
     }
 }
