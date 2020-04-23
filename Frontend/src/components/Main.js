c5e9a9a2891e329c803beee73a705bae931309dc
Arman Pathan <arman.pathan@sjsu.edu>
2020-04-23 16:21:27 -0700

    Added Landing Page and supporting libraries

--- Frontend/src/components/Main.js
+++ Frontend/src/components/Main.js
@@ -1,31 +1,33 @@
-import React, {Component} from 'react';
-import {Route} from 'react-router-dom';
-import Dashboard from './Dashboard/Dashboard';
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import Dashboard from "./Dashboard/Dashboard";
 import UserProfile from "./UserProfile/userProfile";
 import PostTask from "./Task/postTask";
-import Messenger from './Inbox/Messenger'
-import SponsorRating from "./Rating/sponsorRating"
-import InfluencerRating from "./Rating/influencerRating"
-import TaskDescription from './Task/TaskDescription'
-import Appbar from "./Common/Appbar"
-import Sidebar from "./Common/Sidebar"
+import Messenger from "./Inbox/Messenger";
+import SponsorRating from "./Rating/sponsorRating";
+import InfluencerRating from "./Rating/influencerRating";
+import TaskDescription from "./Task/TaskDescription";
+import Appbar from "./Common/Appbar";
+import Sidebar from "./Common/Sidebar";
+import Landing from "./LandingPage/LandingPage";
 
 class Main extends Component {
-    render() {
-        return (
-            <div>
-                <Route path="/" component={Appbar}/>
-                <Route path="/" component={Sidebar} />
-                <Route path="/dashboard" exact component={Dashboard}/>
-                <Route path="/profile" exact component={UserProfile}/>
-                <Route path="/task/new" exact component={PostTask}/>
-                <Route path="/task" exact component={TaskDescription}/>
-                <Route path="/inbox" exact component={Messenger}/>
-                <Route path="/ratings/sponsor" exact component={SponsorRating}/>
-                <Route path="/ratings/influencer" exact component={InfluencerRating}/>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Route path="/" component={Appbar} />
+        <Route path="/" component={Sidebar} />
+        <Route path="/dashboard" exact component={Dashboard} />
+        <Route path="/profile" exact component={UserProfile} />
+        <Route path="/task/new" exact component={PostTask} />
+        <Route path="/task" exact component={TaskDescription} />
+        <Route path="/inbox" exact component={Messenger} />
+        <Route path="/ratings/sponsor" exact component={SponsorRating} />
+        <Route path="/ratings/influencer" exact component={InfluencerRating} />
+        <Route exact path="/" component={Landing} />
+      </div>
+    );
+  }
 }
 
 export default Main;
