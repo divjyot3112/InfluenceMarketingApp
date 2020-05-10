5b7dd7addd3825c5bbc6038dea7e1690f3ee010d
Arman Pathan <arman.pathan@sjsu.edu>
2020-05-10 01:55:57 -0700

    Added Sponsor Analytics Completed Tasks by category

--- Frontend/src/components/analytics/CompletedTasksByCategory.js
+++ Frontend/src/components/analytics/CompletedTasksByCategory.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Bubble } from "react-chartjs-2";
 import { MDBContainer } from "mdbreact";
+import { Pie } from "react-chartjs-2";
 
 import { getSponsorCompletedTasksbyCategory } from "../../actions/analyticsActions";
 import { connect } from "react-redux";
@@ -9,63 +9,89 @@ import { getEmailFromLocalStorage } from "../Common/auth";
 class CompletedTasksByCategory extends React.Component {
   state = {
     email: getEmailFromLocalStorage(),
-
     data: {
       labels: [
-        "Apparels", //pink
-        "Automobile", //purple
-        "Beauty and Personal Care", //orange
-        "Education", //mildgreen
-        "Electronics", //darkpurple
-        "Entertainment", //charcoalgrey
-        "Fitness & Gym", //skyblue
-        "Food", //leafgreen
-        "Health", //yellow
-        "Kids", //lightgreen
-        "Photography", //cyan
-        "Sports & Outdoors", //darkgreen
-        "Travel", // purple
-        "Video Games", //grey
+        // "Apparels", //pink
+        // "Automobile", //purple
+        // "Beauty and Personal Care", //orange
+        // "Education", //mildgreen
+        // "Electronics", //darkpurple
+        // "Entertainment", //charcoalgrey
+        // "Fitness & Gym", //skyblue
+        // "Food", //leafgreen
+        // "Health", //yellow
+        // "Kids", //lightgreen
+        // "Photography", //cyan
+        // "Sports & Outdoors", //darkgreen
+        // "Travel", // purple
+        // "Video Games", //grey
       ],
       datasets: [
         {
-          label: "Completed Tasks By Category",
-          fill: false,
-          lineTension: 0.1,
-          backgroundColor: "rgba(75,192,192,0.4)",
-          borderColor: "rgba(75,192,192,1)",
-          borderCapStyle: "butt",
-          borderDash: [],
-          borderDashOffset: 0.0,
-          borderJoinStyle: "miter",
-          pointBorderColor: "rgba(75,192,192,1)",
-          pointBackgroundColor: "#fff",
-          pointBorderWidth: 1,
-          pointHoverRadius: 5,
-          pointHoverBackgroundColor: "rgba(75,192,192,1)",
-          pointHoverBorderColor: "rgba(220,220,220,1)",
-          pointHoverBorderWidth: 2,
-          pointRadius: 1,
-          pointHitRadius: 10,
           data: [
-            { x: "Apparels", y: 20, r: 5 },
-            { x: 10, y: 20, r: 5 },
-            { x: 10, y: 20, r: 5 },
-            { x: 10, y: 20, r: 5 },
-            { x: 10, y: 20, r: 5 },
-            { x: 10, y: 20, r: 5 },
-            { x: 10, y: 20, r: 5 },
-            { x: 10, y: 20, r: 5 },
-            { x: 10, y: 20, r: 5 },
-            { x: 10, y: 20, r: 5 },
-            { x: 10, y: 20, r: 5 },
+            // 300, 50, 100, 500, 150, 200, 130, 120, 60, 24, 0, 0, 0, 15
           ],
+          backgroundColor: [
+            "#F5A9A9",
+            "#E3CEF6",
+            "#FF8000",
+            "#088A68",
+            "#0A0A2A",
+            "#424242",
+            "#81BEF7",
+            "#4B610B",
+            "#F3F781",
+            "#82FA58",
+            "#01DFD7",
+            "#0B4C5F",
+            "#D0A9F5",
+            "#2E2E2E",
+          ],
+          hoverBackgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
         },
       ],
     },
   };
+
   componentDidMount() {
-    this.props.getSponsorCompletedTasksbyCategory(this.state.email);
+    this.props.getSponsorCompletedTasksbyCategory(this.state.email).then(() => {
+      console.log(
+        "Component did Mount" + this.props.sponsor_CompletedTasksbycategory
+      );
+      const obj = this.props.sponsor_CompletedTasksbycategory;
+      const labels = Object.keys(obj);
+      const data = Object.values(obj);
+      var backgroundColor = [
+        "#F5A9A9",
+        "#E3CEF6",
+        "#FF8000",
+        "#088A68",
+        "#0A0A2A",
+        "#424242",
+        "#81BEF7",
+        "#4B610B",
+        "#F3F781",
+        "#82FA58",
+        "#01DFD7",
+        "#0B4C5F",
+        "#D0A9F5",
+        "#2E2E2E",
+      ];
+      backgroundColor = backgroundColor.slice(0, data.length);
+      const hoverBackgroundColor = ["#FF6384", "#36A2EB", "#FFCE56"];
+      this.setState({
+        data: {
+          labels: labels,
+          datasets: [
+            {
+              data: data,
+              backgroundColor: backgroundColor,
+              hoverBackgroundColor: hoverBackgroundColor,
+            },
+          ],
+        },
+      });
+    });
   }
 
   render() {
@@ -75,7 +101,7 @@ class CompletedTasksByCategory extends React.Component {
     );
     return (
       <MDBContainer style={{ height: "100%", width: "100%" }}>
-        <Bubble data={this.state.data} />
+        <Pie data={this.state.data} />
       </MDBContainer>
     );
   }
